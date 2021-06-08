266c6564a36d4bc79755b7feb55b5de86db30ca6
oherbert <o_herbert@hotmail.com>
2021-06-07 22:18:37 -0300

    Tabela principal criada

--- renderer/app.js
+++ renderer/app.js
@@ -7,64 +7,14 @@ let queryResult = '';
 let departments = [];
 let computador = '';
 let organizacao = '';
-let department = '';
-let refTabela = [];
 
 Promise.resolve(run(query.allDeps))
 .then(res => { 
   departments = res; 
   console.log(departments);
-
   // Tabela de busca
-  const divForm = document.querySelector('.div-form');
-  
-   // Limpa O formulário ao carregar o arquivo 
-   while (divForm.children.length > 0) {
-    divForm.removeChild(divForm.lastChild);
-  }
-  
-  const table = document.createElement('table');
-  table.className = 'content-table';
-
-  const keys = Object.keys(departments[0]);
-
-  let t = '';
-  t += '<thead><tr>';
-  t += keys.map((e) => `<th> ${e.replace('_',' ')} </th>`);
-  t += '</tr></thead>';
-  
-  t += '<tbody>';
-  for(let dep of departments){
-    t += `<tr id='${dep[keys[0]]}' >`;
-
-    refTabela.push(dep[keys[0]]);
-
-    for(let key of keys){
-      
-      t +=  `<td class="active-row">`;
-      
-      t += (dep[key] === null ) ? '-': dep[key];
-      
-      t+= '</td>';
-    }
-    t+= '</tr>';
-  }
-  t += '</tbody>';
-
-  table.innerHTML = t.replace(/,/g,'');
-  divForm.appendChild(table);
-}).finally(()=>{ 
-
-  refTabela.forEach(element => {
-    document.getElementById(element).addEventListener ("click", e => {callBtn(element)});
-  })
-
+  createTable('.div-form', departments, true);  
 });
-
-function callBtn(e){
-  depTxt.value = e;
-  btnFind.click();
-}
 
 // Dom Nodes
 let showModal = document.getElementById('show-modal'),
@@ -107,8 +57,21 @@ btnFind.addEventListener('click', e => {
   
   Promise.resolve(run(query.allEmps, depTxt.value))
   .then(res => { 
-  department = res;
-  console.log(department); 
+  const emps = res;
+  
+  if(emps.length > 0) createTable('.emps',emps);
+  else{
+    const div = document.querySelector('.emps');
+    const p = document.createElement('p');
+    p.id = 'no-items';
+    p.innerHTML = 'Sem resultado';    
+    while (div.children.length > 0) {
+       div.removeChild(div.lastChild);
+    }
+    
+    div.appendChild(p);
+  }
+
   })
   .finally(() => {
     toggleModalButtons();
@@ -128,8 +91,8 @@ ipcRenderer.once('params', ( e ,parms) => {
   parms.forEach(element => {
    if(element.indexOf('=') > 0){ 
      const value = element.split(',');
-     value.forEach(e => {
-        const val = e.split('=');
+     value.forEach(ev => {
+        const val = ev.split('=');
         if( val[0].toUpperCase() === 'COMPUTADOR') computador = val[1];
         if( val[0].toUpperCase() === 'ORGANIZACAO') organizacao = val[1]; 
      });
@@ -142,3 +105,58 @@ Promise.resolve(run(query.usuario, computador, organizacao))
     if (queryResult.length === 0 || computador != os.hostname) ipcRenderer.send('User-Proibido');
   });
 })
+
+
+function createTable(div, array ,addClick = false){
+  let refTabela = [];
+  const divForm = document.querySelector(div);
+  
+   // Limpa O formulário ao carregar o arquivo 
+   while (divForm.children.length > 0) {
+    divForm.removeChild(divForm.lastChild);
+  }
+  
+  const table = document.createElement('table');
+  table.className = 'content-table';
+
+  const keys = Object.keys(array[0]);
+
+  // Cria cabeçalho da tabela
+  let t = '';
+  t += '<thead><tr>';
+  t += keys.map((e) => `<th> ${e.replace('_',' ')} </th>`);
+  t += '</tr></thead>';
+  
+  // Cria corpo da tabela
+  t += '<tbody>';
+  for(let val of array){
+    t += (addClick) ? `<tr id='${val[keys[0]]}'>` : `<tr>`;
+
+    refTabela.push(val[keys[0]]);
+
+    for(let key of keys){
+      
+      t +=  `<td class="active-row">`;
+      
+      t += (val[key] === null ) ? '-': val[key];
+      
+      t+= '</td>';
+    }
+    t+= '</tr>';
+  }
+  t += '</tbody>';
+
+  table.innerHTML = t.replace(/,/g,'');
+  divForm.appendChild(table);
+
+  if(addClick){
+  refTabela.forEach(element => {
+    document.getElementById(element).addEventListener ("click", e => {callBtn(element)});
+  })
+  }
+}
+
+function callBtn(e){
+  depTxt.value = e;
+  btnFind.click();
+}
--- repository/query.js
+++ repository/query.js
@@ -10,9 +10,14 @@ static allDeps = `SELECT department_id, department_name, manager_id
                  FROM departments
                  ORDER BY department_id`;
 
-static allEmps = `SELECT * 
+static allEmps = `SELECT e.employee_id,
+                 e.first_name,
+                 e.last_name,
+                 e.email,
+                 e.phone_number,
+                 e.job_id,
+                 e.salary
                  FROM hr.employees e
-                 WHERE e.department_id = :dep
-                 ORDER BY e.employee_id` 
+                 WHERE e.department_id = :dep` 
 }
 module.exports = Query;
